d987f6d8e8016f4bcd70635445e874ae6c719b79
Dipanjan Pal <dipanjanpal@hotmail.com>
2020-04-01 13:43:54 +0530

    show right blank and wrong words to user in scoreboard

--- Test/ScoreboardViewController.swift
+++ Test/ScoreboardViewController.swift
@@ -10,21 +10,36 @@ import UIKit
 
 class ScoreboardViewController: UIViewController {
 
+    var arrWords = [String]()
     @IBOutlet weak var lblScore: UILabel!
+    @IBOutlet weak var collectionMain: UICollectionView!
+    @IBOutlet weak var collectionLayout: UICollectionViewFlowLayout!{
+        didSet {
+            collectionLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let nib1 = UINib(nibName: Constants.reuseID, bundle: Bundle.main)
+        collectionMain.register(nib1, forCellWithReuseIdentifier: Constants.reuseID)
+        
         var arrUserInputtedWordsKeys = Array(CommonConstants.shared.arrUserInputtedWords.keys)
         arrUserInputtedWordsKeys.sort(by: <)
         lblScore.text = "Score\n\(evaluate(keysForUSerInputes: arrUserInputtedWordsKeys))"
-        
+        collectionMain.reloadData()
     }
     
     func evaluate(keysForUSerInputes :[Int]) -> Int{
         var index = 0
         var score = 0
-        for i in keysForUSerInputes{
-            if CommonConstants.shared.arrUserInputtedWords[i] ==  CommonConstants.shared.arrActualRemovedWords[index]{
+        for keyNumber in keysForUSerInputes{
+            if CommonConstants.shared.arrUserInputtedWords[keyNumber] ==  CommonConstants.shared.arrActualRemovedWords[index]{
                 score += 1
+                arrWords[keyNumber] = CommonConstants.shared.arrActualRemovedWords[index]
+            }
+            else{
+                arrWords[keyNumber] = "*\(CommonConstants.shared.arrActualRemovedWords[index]) / \(CommonConstants.shared.arrUserInputtedWords[keyNumber] ?? "")"
             }
             index += 1
         }
@@ -39,4 +54,39 @@ class ScoreboardViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
+}
+
+extension ScoreboardViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arrWords.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.reuseID, for: indexPath) as! CollectionViewCell
+        cell.lblWords.text = nil
+        cell.tfWord.text = nil
+        cell.indexPathRow = indexPath.item
+        let word = arrWords[indexPath.item]
+        
+        cell.lblWords.isHidden = false
+        cell.tfWord.isHidden = true
+        cell.lblWords.text = word
+        if word.contains("*"){
+            cell.lblWords.backgroundColor = UIColor.red
+        }
+        else{
+            cell.lblWords.backgroundColor = UIColor.init(red: 239/255, green: 239/255, blue: 239/255, alpha: 1)
+        }
+        
+        cell.layer.borderWidth = 0
+        cell.layer.borderColor = UIColor.clear.cgColor
+        
+        return cell
+    }
+}
+
+private enum Constants {
+    static let reuseID = "CollectionViewCell"
+    static let scoreBoardID = "ScoreboardViewController"
 }
