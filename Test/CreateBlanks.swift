d0a97fe3b3d3a122f377f2daa66161148aa2a528
Dipanjan Pal <dipanjanpal@hotmail.com>
2020-04-01 09:13:31 +0530

    initial setup

---
+++ Test/CreateBlanks.swift
@@ -0,0 +1,82 @@
+//
+//  CreateBlanks.swift
+//  Test
+//
+//  Created by Dipanjan Pal on 31/03/20.
+//  Copyright © 2020 Dipanjan Pal. All rights reserved.
+//
+
+import Foundation
+
+class CreateBlanks{
+    let para = "Wine is an alcoholic drink typically made from fermented grapes. Yeast consumes the sugar in the grapes and converts it to ethanol, carbon dioxide, and heat. Different varieties of grapes and strains of yeasts produce different styles of wine. These variations result from the complex interactions between the biochemical development of the grape, the reactions involved in fermentation, the grape is growing environment (terroir), and the production process. Many countries enact legal appellations intended to define styles and qualities of wine. These typically restrict the geographical origin and permitted varieties of grapes, as well as other aspects of wine production. Wines not made from grapes involve fermentation of additional crops including, rice wine and other fruit wines such as plum, cherry, pomegranate, currant and elderberry. Wine has been produced for thousands of years.The earliest evidence of wine is from Georgia (6000 BC), Iran (5000 BC), and Sicily (4000 BC). In modern times, the five countries with the largest wine-producing regions are in Italy, Spain, France, the United States, and China."
+    
+    let varbsFilter = ["am", "is", "are", "was", "were"]
+
+    let tagger = NSLinguisticTagger(tagSchemes:[.tokenType, .language, .lexicalClass, .nameType, .lemma], options: 0)
+    let options: NSLinguisticTagger.Options = [.omitPunctuation, .omitWhitespace, .joinNames]
+
+    func createWordsCollection() -> [String]{
+        let arrPara = para.split(separator: ".")
+        var arrWords = [String]()
+        //var arrLines = [[String]]()
+        for line in arrPara{
+            removeVerbs(for: String(line)).forEach { (word) in
+                arrWords.append(word)
+            }
+        }
+        
+        //print(arrWords)
+        return arrWords
+    }
+    
+//    func createLines() -> [String]{
+//        let arrLines = createWordsCollection()
+//        var arrLinesCollection = [String]()
+//        for item in arrLines{
+//            var arrTemp = [String]()
+//            item.forEach { (word) in
+//                if word == "_"{
+//                    let line = arrTemp.joined(separator: " ")
+//                    arrLinesCollection.append(line)
+//                    arrLinesCollection.append("_")
+//                    arrTemp.removeAll()
+//                }
+//                else if word == "."{
+//                    arrTemp.append(word)
+//                    let line = arrTemp.joined(separator: " ")
+//                    arrLinesCollection.append(line)
+//                    arrTemp.removeAll()
+//                }
+//                else{
+//                    arrTemp.append(word)
+//                }
+//                
+//            }
+//        }
+//        return arrLinesCollection
+//    }
+    
+    
+    func removeVerbs(for text: String) -> [String]{
+        var newItem = [String]()
+        tagger.string = text
+        let range = NSRange(location: 0, length: text.utf16.count)
+        tagger.enumerateTags(in: range, unit: .word, scheme: .lexicalClass, options: options) { tag, tokenRange, _ in
+            
+            if let tag = tag {
+                let word = (text as NSString).substring(with: tokenRange)
+                if (tag.rawValue == "Verb" && varbsFilter.contains(word) != true && newItem.contains("_") != true){
+                    newItem.append("_")
+                }
+                else{
+                     newItem.append(word)
+                }
+                //print("\(word): \(tag.rawValue)")
+            }
+        }
+        newItem.append(".")
+        return newItem
+    }
+
+}
--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var arrWords = [String]()
+    
+    @IBOutlet weak var collectionMain: UICollectionView!
+    @IBOutlet weak var collectionLayout: UICollectionViewFlowLayout!{
+        didSet {
+            collectionLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let nib1 = UINib(nibName: Constants.reuseID, bundle: Bundle.main)
+        collectionMain.register(nib1, forCellWithReuseIdentifier: Constants.reuseID)
+        
+        let objItems = CreateBlanks()
+        arrWords = objItems.createWordsCollection()
+        collectionMain.reloadData()
     }
 
 
 }
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arrWords.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: Constants.reuseID, for: indexPath) as! CollectionViewCell
+        
+        let word = arrWords[indexPath.item]
+        if word != Constants.blank{
+            cell.lblWords.isHidden = false
+            cell.tfWord.isHidden = true
+            cell.lblWords.text = word
+        }
+        else{
+            cell.lblWords.isHidden = true
+            cell.tfWord.isHidden = false
+            cell.tfWord.text =  "         "
+            cell.tfWord.isUserInteractionEnabled = true
+            cell.layer.borderWidth = Constants.borderWidth
+            cell.layer.borderColor = UIColor.lightGray.cgColor
+        }
+        
+        //cell.maxWidth = collectionView.bounds.width - Constants.spacing
+        return cell
+    }
+}
+
+private enum Constants {
+    static let spacing: CGFloat = 16
+    static let borderWidth: CGFloat = 0.5
+    static let reuseID = "CollectionViewCell"
+    static let blank = "_"
+}
+
+
 
